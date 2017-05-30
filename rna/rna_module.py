b1b6638d66b3d7430668524ddcc91cecdc1f4cbe
CristianoSouza <cristianoantonio.souza10@gmail.com>
2017-05-30 15:49:28 -0300

    conflit

--- rna/rna_module.py
+++ rna/rna_module.py
@@ -43,9 +43,6 @@ class RnaModule(object):
 		csv_logger = CSVLogger('training.log')
 		
 
-		x = keras.preprocessing.text.Tokenizer(num_words=None, filters= filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', lower=True, split=" ", char_level=False)
-		print(x)
-		exit()
 		fit = self.model.fit(self.data_set_samples, self.data_set_labels, nb_epoch=150, batch_size=10, callbacks=[csv_logger])
 
 	def predict(self):
@@ -113,6 +110,9 @@ class RnaModule(object):
 
 	def getNumberNeuronsImputLayer(self):
 		return self.imput_dim_neurons
+
+	def setDimImputLayer(self, dim_imput_layer):
+		self.dim_imput_layer = dim_imput_layer
 
 	def getDimImputLayer(self):
 		return self.dim_imput_layer 
