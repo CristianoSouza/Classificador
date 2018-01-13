2729ba6e7af5e23f1e892541da7d1db11f65c319
Willian Zalewski <wzalewski@cluster.bad>
2018-01-13 01:46:06 -0200

    update_bases

--- job_partition.sh
+++ job_partition.sh
@@ -9,4 +9,4 @@
 #SBATCH -n 1	 
 export PATH="/home/wzalewski/anaconda3/bin:$PATH"
 source activate py27tensorflow
-srun python main.py
+srun python main_30.py
---
+++ job_partition12.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -p 7d
+#SBATCH -t 7-00:00:00
+#SBATCH --job-name=part_20
+#SBATCH -o pre_processamento_partition.out
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=cristianoantonio.souza10@gmail.com
+#SBATCH -n 1	 
+export PATH="/home/wzalewski/anaconda3/bin:$PATH"
+source activate py27tensorflow
+srun python main_12.py
